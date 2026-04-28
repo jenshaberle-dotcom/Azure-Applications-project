11b642c3e62e3528105371ea4e4bb012a7ada9c5
jenshaberle-dotcom <jens.haberle@gmail.com>
2026-04-28 14:09:53 +0200

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,9 +7,11 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 logging.basicConfig(level=logging.INFO)
 app.logger.setLevel(logging.INFO)
