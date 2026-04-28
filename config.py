d7248098065ecda0099a26e8bbf908c283cd78d2
jenshaberle-dotcom <jens.haberle@gmail.com>
2026-04-28 18:56:53 +0200

    Update sensitive configuration values in config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "9f3c2a7e1b8d4f6a0c5e9b2d7a1f4c8e6b3d0a9f2c7e1b5d4f6a8c2e7b1d9f3"
 
     BLOB_ACCOUNT = os.environ.get("BLOB_ACCOUNT") or "azureappsjensstore"
-    BLOB_STORAGE_KEY = os.environ.get("BLOB_STORAGE_KEY") or "o5iqvvL4QY+zwHDmRCA+l1rnRjOfBa4QUEXWaOrdrCmQoQETl4Z0Y+55KTpkP41YrFyeicJqmG9u+AStXAXGoA=="
+    BLOB_STORAGE_KEY = os.environ.get("BLOB_STORAGE_KEY") or "fiDrfgES4JD+O10qXlw7sbdI0lVZwp6VOi3cV3XwT+pio2j1PneyKF9sioN9+uaY2o0GPeza3EIx+AStLQYlmw=="
     BLOB_CONTAINER = os.environ.get("BLOB_CONTAINER") or "article-images"
 
     SQL_SERVER = os.environ.get("SQL_SERVER") or "azureapps-jens-sqlsrv.database.windows.net"
@@ -26,9 +26,9 @@ class Config(object):
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
 
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET") or "OLV8Q~9h~ez0DWuIUrBlE42SZ~5zoMu5Nl4YUbD4"
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET") or "MES8Q~3gqlUobVMmJ7ylw.yQnDm1uQU2KqnS4bFt"
     AUTHORITY = os.environ.get("AUTHORITY") or "https://login.microsoftonline.com/f958e84a-92b8-439f-a62d-4f45996b6d07"
-    CLIENT_ID = os.environ.get("CLIENT_ID") or "fae0df4e-1f7a-4c98-80b8-0838c4f07eb7"
+    CLIENT_ID = os.environ.get("CLIENT_ID") or "6f4094f3-cc77-4e36-9f6a-862a685fe1f9"
 
     REDIRECT_PATH = "/getAToken"
     SCOPE = ["User.Read"]
