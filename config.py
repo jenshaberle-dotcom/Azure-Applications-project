d1d7c90054cf52af4527d084d26e44eebff6632a
jenshaberle-dotcom <jens.haberle@gmail.com>
2026-04-28 13:29:39 +0200

    Update configuration values for Azure services

--- config.py
+++ config.py
@@ -1,47 +1,35 @@
 import os
+from urllib.parse import quote_plus
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "9f3c2a7e1b8d4f6a0c5e9b2d7a1f4c8e6b3d0a9f2c7e1b5d4f6a8c2e7b1d9f3"
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.environ.get("BLOB_ACCOUNT") or "azureappsjensstore"
+    BLOB_STORAGE_KEY = os.environ.get("BLOB_STORAGE_KEY") or "o5iqvvL4QY+zwHDmRCA+l1rnRjOfBa4QUEXWaOrdrCmQoQETl4Z0Y+55KTpkP41YrFyeicJqmG9u+AStXAXGoA=="
+    BLOB_CONTAINER = os.environ.get("BLOB_CONTAINER") or "article-images"
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'azureapps-jens-sqlsrv.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'CMS4dmin'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'azurejensadmin'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'CMS4dmin'
-    # Below URI may need some adjustments for driver version, based on your OS, if running locally
-   SQLALCHEMY_DATABASE_URI = (
-    'mssql+pyodbc://'
-    + SQL_USER_NAME + ':' + SQL_PASSWORD
-    + '@' + SQL_SERVER + ':1433/'
-    + SQL_DATABASE
-    + '?driver=ODBC+Driver+17+for+SQL+Server'
-)
+    SQL_SERVER = os.environ.get("SQL_SERVER") or "azureapps-jens-sqlsrv.database.windows.net"
+    SQL_DATABASE = os.environ.get("SQL_DATABASE") or "azureapps-cms-db"
+    SQL_USER_NAME = os.environ.get("SQL_USER_NAME") or "azurejensadmin"
+    SQL_PASSWORD = os.environ.get("SQL_PASSWORD") or "CMS4dmin"
+
+    SQLALCHEMY_DATABASE_URI = (
+        "mssql+pyodbc://"
+        f"{SQL_USER_NAME}:{quote_plus(SQL_PASSWORD)}"
+        f"@{SQL_SERVER}:1433/{SQL_DATABASE}"
+        "?driver=ODBC+Driver+17+for+SQL+Server"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
-    # In your production app, Microsoft recommends you to use other ways to store your secret,
-    # such as KeyVault, or environment variable as described in Flask's documentation here:
-    # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
-    # CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-    # if not CLIENT_SECRET:
-    #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
-    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET") or "OLV8Q~9h~ez0DWuIUrBlE42SZ~5zoMu5Nl4YUbD4"
+    AUTHORITY = os.environ.get("AUTHORITY") or "https://login.microsoftonline.com/f958e84a-92b8-439f-a62d-4f45996b6d07"
+    CLIENT_ID = os.environ.get("CLIENT_ID") or "fae0df4e-1f7a-4c98-80b8-0838c4f07eb7"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
-
-    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
-
-    # You can find the proper permission names from this document
-    # https://docs.microsoft.com/en-us/graph/permissions-reference
-    SCOPE = ["User.Read"] # Only need to read user profile for this app
-
-    SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
+    REDIRECT_PATH = "/getAToken"
+    SCOPE = ["User.Read"]
+    SESSION_TYPE = "filesystem"
